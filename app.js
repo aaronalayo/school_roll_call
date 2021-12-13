e99d3f54599e4cfca51a7c77c423fe5790566dfe
aaronalayo <aaron.aa@me.com>
2021-12-13 13:14:05 +0100

    Added acces to login route

--- app.js
+++ app.js
@@ -3,42 +3,27 @@ import http from "http";
 
 // Setup Objection + Knex
 
-import objection from "objection";
-import Knex from "knex";
-import connection from "./knexfile.js";
+
 const app = express();
 const server = http.createServer(app);
+
+app.use(express.static("public"));
+
+import connection from "./knexfile.js";
+import objection from "objection";
+import Knex from "knex";
+
 const { Model } = objection;
 const knex = Knex(connection.development);
-knex.on("query", function (queryData) {
-	console.log(queryData);
-});
+// knex.on("query", function (queryData) {
+// 	console.log(queryData);
+// });
 
 Model.knex(knex);
 
-// import School from './model/School.js';
 
-//async function getPublicIp(){
-
-//     console.log(await publicIp.v4());
-//     //=> '46.5.21.123'
-
-//     console.log(await publicIp.v6());
-//     //=> 'fe80::200:f8ff:fe21:67cf'
-
-//}
-
-app.get("/", async (req, res) => {
-//   const myIp = ip.address('public', 'ipv4')
-//   const school = await School.query().select().where({school_ip : '94.18.243.162'});
-//  getPublicIp().then((data)=>{
-//   //  console.log(data)
-//     const school = School.query().select().where({school_ip : data});
-//     const result = ip.isEqual(data, school[0].school_ip)
-//     console.log(result)
-
-	res.send("Hello");
-});
+import { router } from "./app/routes/router.js";
+app.use(router);
 
 const port = process.env.PORT ? process.env.PORT : 8080;
 const port2 = "0.0.0.0";
---
+++ app/routes/router.js
@@ -0,0 +1,45 @@
+"use strict";
+import express from "express";
+let router = express.Router();
+
+router.use(express.static("public"));
+import * as fs from "fs";
+import School from "../model/School.js";
+import getPublicIp from "../middleware/getPublicIp.js";
+import checkIp from "../middleware/checkIp.js";
+
+const homePage = fs.readFileSync("./public/homepage.html", "utf8");
+
+router.get("/", async (req, res) =>{
+	return res.send(homePage);
+});
+
+router.get("/login", async (req, res) => {
+	const studentIp = await getPublicIp();
+	console.log(studentIp);
+	if(!studentIp || typeof studentIp == undefined){
+		res.send("There is no internet connection");
+	}else{
+		const school = await School.query().select().where({school_ip: studentIp});
+		if(!school || typeof school == undefined ){
+			res.send("This school doesnt exits").redirect("/");
+		}else{
+			const result = checkIp(studentIp, school[0].school_ip);
+			console.log(result);
+			if(result == false){
+				res.send("You are not at school").redirect("/");
+			}else{
+				res.send("Welcome student");
+			}
+		}
+	}
+
+   
+ 
+    
+    
+    
+
+});
+
+export { router };
