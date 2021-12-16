68bc2e87f0f147488af2699b4191a961e5e54481
akQuadSat <ak@quadsat.com>
2021-12-16 12:22:52 +0100

    Removed front-end

--- roll-call-fron-end/roll-call/src/pages/homePage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const HomePage = () => (
-    <>
-    <h1>Welcome to your SchoolRollCall</h1>
-    <a href="/login">Click here to log-in</a>
-    </>
-);
-export default HomePage;
