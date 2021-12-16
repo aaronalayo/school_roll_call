3de448390ffd9fd7d82cbd9ce44cbd5409d75ff2
akQuadSat <ak@quadsat.com>
2021-12-16 15:57:14 +0100

    Adding integration tests

--- tests_integration/codeGenerator.test.js
+++ tests_integration/codeGenerator.test.js
@@ -5,7 +5,6 @@ import Knex from "knex";
 import objection from "objection";
 import connection from "../knexfile.js";
 
-
 const knex = Knex(connection.development);
 const { Model } = objection;
 Model.knex(knex);
@@ -14,7 +13,17 @@ afterAll(() => {
 	knex.destroy();
 });
 
-test("codeGenerator test", async () => {
+test("codeGenerator return code test", async () => {
 	const code = await codeGenerator();
 	expect(code !== 0);
+});
+
+test("codeGenerator code lenght", async () => {
+	const code = await codeGenerator();
+	expect(code.lenght !== 6);
+});
+
+test("codeGenerator code dublicate code", async () => {
+	const code = await codeGenerator();
+	expect(code.lenght !== 6);
 });
