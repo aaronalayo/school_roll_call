261d58fbe44b7e72d3984794a95aca443945b755
aaronalayo <aaron.aa@me.com>
2022-01-10 12:54:44 +0100

    Changed generate to minutes

--- back-end/app/middleware/getGenerateInfo.js
+++ back-end/app/middleware/getGenerateInfo.js
@@ -2,22 +2,21 @@
 import Subject from "../model/Subject.js"; 
 import Code from "../model/Code.js";
 import codeGenerator from "./codeGenerator.js";
+import moment from "moment";
 
 
 export default async function getGenerateInfo(subjectId, userId, expirationTime) {
 	const subject = await Subject.query().select().where({subject_uuid: subjectId}).withGraphFetched("programs");
 
 	const code = await codeGenerator();
-	let date = new Date().getTime();
-	let expTime = date + Number(expirationTime) * 1000 + new Date().getTimezoneOffset();
-	expTime =  new Date(expTime);
-	let duration = expTime - date;
-	duration = Math.round(duration / 1000);
+	
+	let expTime = moment().add(Number(expirationTime), "minutes") ;
+
 	await Code.query().insert({code: code, subject_uuid: subject[0].subject_uuid, expires_at: expTime});
 	
 	const generatedInfo = {
 		"subjectName": subject[0].subject_name,
-		"expirationTime": duration,
+		"expirationTime": Number(expirationTime),
 		"code": code
 	};
 	return generatedInfo;   
--- back-end/scheduler.js
+++ back-end/scheduler.js
@@ -1,11 +1,11 @@
 import connection from "./knexfile.js";
 import Knex from "knex";
-import Code from "./app/model/Code.js"
+import Code from "./app/model/Code.js";
 const knex = Knex(connection.development);
 
 deleteOldRows();
 export default async function deleteOldRows() {
-    console.log("executing scheduler")
+	console.log("executing scheduler");
 	await knex.raw("DELETE FROM codes WHERE expires_at < NOW() - INTERVAL '1 minute';");
 
 }
